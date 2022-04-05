14b6c5f24e70d5bc4b1f02e16d92ec05f48a1478
Afsana Ara Bristy <afsanaara928@gmail.com>
2022-04-05 22:25:14 +0600

    blogs done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const Blogs = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h1>what's the context API?</h1>
+      <p>
+        It's a way to move props from grand parent to child to parent.It returns
+        a consumer and a provider. Provider is a component that as it's names
+        suggests provides the state to its children. Context API is used when
+        some data needs to be accessible by many components at different nesting
+        levels.
+      </p>
+
+      <h1>What is semantic tag? </h1>
+            <p>In HTML,the "h1" tag is a semantic tag. Which gives the text it wraps around the role of "a top level heading on your page." There are some semantic tag available:</p>
+            <p>1.article</p>
+            <p>2.header</p>
+            <p>3.footer</p>
+            <p>4.main</p>
+            <p>5.nav</p>
+            <p>6.section</p>
+            <p>7.aside</p>
+    </div>
+  );
 };
 
 export default Blogs;
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            
+           <img src="https://img.freepik.com/free-vector/error-404-concept-landing-page_52683-12870.jpg?t=st=1649175020~exp=1649175620~hmac=31266132d8af7e67245f6787000017f813b0e508ff07a03a9094f8574f14d925&w=740" alt="" /> 
         </div>
     );
 };
